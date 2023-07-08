405cfded460b4f54c82910067660bf185272c2ac
Kevin Choi <kevinnwjchoi@gmail.com>
2023-07-08 14:07:50 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName){
+    const newMessages = [];
+    for (let i = 0; i < names.length; i++){
+        newMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newMessages;
+    
+}
+
+function countDown(number){
+    for (let i = number; i >= 0; i--){
+        console.log(i);
+    }
+     
+} 
